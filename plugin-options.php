581b823df22c4ba91da63c32fbe668ea0aea9e25
Eric Allen <eric@ericrallen.com>
2012-08-18 00:41:45 -0400

    Updated version number.

--- plugin-options.php
+++ plugin-options.php
@@ -5,7 +5,7 @@
 	
 	global $options, $shortcode, $caps;
 	$options = array(
-		'ia_jsfiddle_version_number' => '0.3b',
+		'ia_jsfiddle_version_number' => '1.0b',
 		'ia_jsfiddle_username_field' => 'iajsfiddle',
 		'ia_jsfiddle_skins_dir' => plugins_url() . '/jsfiddle-plugin/assets/jsFiddle-skin-proxy/skins/',
 		'ia_jsfiddle_theme_manager_cron' => 'hourly'
