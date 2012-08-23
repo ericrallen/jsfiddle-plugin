09d6abcd699b7fe21677f1735d13b1d357f138e1
Eric Allen <decoy@internetalche.me>
2012-08-22 21:06:46 -0400

    Removed extra folder in plug-in structure because I didn't understand the whole WP Plug-in Repo thing.

--- jsfiddle-shortcode-w-custom-skins/assets/jsFiddle-skin-proxy/skins/ia-jsfiddle-skin-manager.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	require_once('ia-jsfiddle-skin-manager.class.php');
-	//generates a JSON file containing JSFiddle embed skins
-	$skin_manager = new IA_JSFiddle_Skin_Manager();
-	$skin_manager->get_themes();
-
-?>
