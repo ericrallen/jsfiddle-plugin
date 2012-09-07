b6106e87cafd2a38cf758a4060f7b7e9e49ae5f6
Eric Allen <decoy@internetalche.me>
2012-09-06 22:18:16 -0400

    Updated plug-in to fix directory issue.

--- assets/classes/IAJSFiddle.class.php
+++ assets/classes/IAJSFiddle.class.php
@@ -39,7 +39,7 @@
 			$show_string = substr($show_string,0,-1);
 			if(!is_feed()) {
 				if($this->skin && $this->skin !== 'default') {
-					$this->output .= '<iframe class="ia-jsfiddle-embed ia-jsfiddle-embed-' . $this->skin . '" style="width: ' . $this->width . '; height: ' . $this->height . ';" src="' . plugins_url() . '/jsfiddle-plugin/assets/jsFiddle-skin-proxy/?id=' . $this->fiddle['code'] . '&tabs=' . $this->show .'&skindir=' . get_option('ia_jsfiddle_skins_dir') . '&skin=' . $this->skin . '" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
+					$this->output .= '<iframe class="ia-jsfiddle-embed ia-jsfiddle-embed-' . $this->skin . '" style="width: ' . $this->width . '; height: ' . $this->height . ';" src="' . plugins_url() . '/jsfiddle-shortcode-w-custom-skins/assets/jsFiddle-skin-proxy/?id=' . $this->fiddle['code'] . '&tabs=' . $this->show .'&skindir=' . get_option('ia_jsfiddle_skins_dir') . '&skin=' . $this->skin . '" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
 				} else {
 					$this->output .= '<iframe class="ia-jsfiddle-embed ia-jsfiddle-embed-default" style="width: ' . $this->width . '; height: ' . $this->height . ';" src="http://jsfiddle.net/' . $this->fiddle['user'] . '/' . $this->fiddle['code'] . '/embedded/' . $this->show . '" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
 				}
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ericrallen
 Tags: jsfiddle, shortcode
 Requires at least: 3.4.0
 Tested up to: 3.4
-Stable tag: 1.2
+Stable tag: 1.3
 License: MIT
 License URI: http://opensource.org/licenses/mit-license.php
 
